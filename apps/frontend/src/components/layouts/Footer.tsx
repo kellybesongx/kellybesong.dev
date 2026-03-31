ee5f3c70622abc273939812a9e92cebd2bca6450
Kelwilson <kelwilson_achienu@icloud.com>
2026-03-31 14:24:47 -0700

    update: hero section icons and contents

---
+++ apps/frontend/src/components/layouts/Footer.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Footer() {
+  return (
+    <footer className="bg-white/80 backdrop-blur-md">
+      <div className="text-center py-4 text-xs text-gray-500">
+        © {new Date().getFullYear()} Kelly-Besong.DEV.
+      </div>
+    </footer>
+  )
+}
